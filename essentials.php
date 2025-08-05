f88c16b4d0ab4810c44c53b0ea6e9a583291230c
Mohammad Hamza Khan <humza.kingdomvision@gmail.com>
2025-08-05 18:06:57 +0500

    add condition to add logs dir if doesn't esixts

--- essentials.php
+++ essentials.php
@@ -13,13 +13,18 @@ if (!function_exists('pre')) {
 
 if (!function_exists('cf_log')) {
     function cf_log($data, $filename = 'log_cronjob', $fileext = 'txt' ){
-        $filePath = dirname(__FILE__) . '/logs/' . $filename.'.'.$fileext;
+        $directory = dirname(__FILE__) . '/logs/';
+        $filePath =  $directory. $filename.'.'.$fileext;
         $data = print_r($data, true);
         $data .= ' -- -- -- ' . current_time('mysql');
         $data .= '<br>';
         ob_start();
         pre( $data );
         $data = ob_get_clean();
+
+        if (!is_dir($directory)) {
+            mkdir($directory, 0777, true); // 0777 for full permissions, true for recursive
+        }
         $myfile = file_put_contents($filePath, (string) $data . PHP_EOL, FILE_APPEND | LOCK_EX);
     }
 }
