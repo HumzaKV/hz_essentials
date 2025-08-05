ce7493c7b89745fddc1cbd3d1fce3d5ac8125cdf
Mohammad Hamza Khan <humza.kingdomvision@gmail.com>
2025-08-05 17:33:12 +0500

    modified function cf_log

--- essentials.php
+++ essentials.php
@@ -1,14 +1,5 @@
 <?php
-// Version: 1.0.0
-if (!function_exists('cf_log')) {
-    function cf_log($data, $filename = 'log_cronjob', $fileext = 'txt' )
-    {
-        $filePath = dirname(__FILE__) . '/logs/' . $filename.'.'.$fileext;
-        $data     = print_r($data, true);
-        $data .= ' -- -- -- ' . current_time('mysql');
-        $myfile = file_put_contents($filePath, (string) $data . PHP_EOL, FILE_APPEND | LOCK_EX);
-    }
-}
+// Version: 1.0.1
 
 if (!function_exists('pre')) {
     function pre($data, $die = 0) {
@@ -17,5 +8,18 @@ if (!function_exists('pre')) {
             if ($die)
                 die;
         }
+    }
+}
+
+if (!function_exists('cf_log')) {
+    function cf_log($data, $filename = 'log_cronjob', $fileext = 'txt' ){
+        $filePath = dirname(__FILE__) . '/logs/' . $filename.'.'.$fileext;
+        $data = print_r($data, true);
+        $data .= ' -- -- -- ' . current_time('mysql');
+        $data .= '<br>';
+        ob_start();
+        pre( $data );
+        $data = ob_get_clean();
+        $myfile = file_put_contents($filePath, (string) $data . PHP_EOL, FILE_APPEND | LOCK_EX);
     }
 }
